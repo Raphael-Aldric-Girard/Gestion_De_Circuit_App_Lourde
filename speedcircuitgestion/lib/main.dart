2711cc92d2fd10ab5b540a69d23c11b1f3e04891
Terry <terry.douteau17@gmail.com>
2025-12-04 16:26:04 +0100

    création de la page de connexion

--- speedcircuitgestion/lib/main.dart
+++ speedcircuitgestion/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'api_service.dart';
+import 'evenement.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,12 +32,51 @@ class _AuthScreenState extends State<AuthScreen> {
     final String password = _passwordController.text;
 
     try {
-      final result = await ApiService.login(identifiant, password);
+      final result = await ApiService().login(identifiant, password);
       if (result.containsKey('message')) {
         Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => )
-        )
+          MaterialPageRoute(builder: (context) => EvenementPage())
+        );
+      } else {
+        setState(() {
+          _errorMessage = 'Echec de la connexion : ${result['error']}';
+        });
       }
+    } catch (error) {
+      setState(() {
+        _errorMessage = 'Erreur de connexion au serveur';
+      });
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Connexion')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _identifiantController,
+              decoration: InputDecoration(labelText: 'Identifiant'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Mot de passe'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _login, child: Text('Se conncter')),
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Text(_errorMessage, style: TextStyle(color: Colors.red)),
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
